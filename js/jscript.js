7b0e4626b2622f782a2e8a0236da380db3f7bd19
martin01fer <fbaudinsubils.redes@gmail.com>
2022-07-04 03:17:24 -0300

    Primer comit

---
+++ js/jscript.js
@@ -0,0 +1,19 @@
+function cambiarfondo(){
+    document.body.style.backgroundColor = "red";
+}
+function cambiarfondo2(){
+    document.body.style.backgroundColor = "#FFF";
+}
+function cartel(){
+    alert("Hola Potrero");
+}
+function AmpliarImg(){
+    X.style.heigth = "20%";
+    X.style.width = "20%";
+
+}
+function Normalmg(){
+    X.style.heigth = "10%";
+    X.style.width = "10%";
+
+}
